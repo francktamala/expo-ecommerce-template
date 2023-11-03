f69eec0abd5645c334835907b993ad24e911e69f
francktamala <75437467+francktamala@users.noreply.github.com>
2023-11-04 01:15:04 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,15 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBW84ML6OU4_9wOR34lNK1SYftxXsrEepk",
-    authDomain: "shop-8ced3.firebaseapp.com",
-    projectId: "shop-8ced3",
-    storageBucket: "shop-8ced3.appspot.com",
-    messagingSenderId: "1090323738531",
-    appId: "1:1090323738531:web:e15112619d9266013cf416",
-    measurementId: "G-XKKRFRTZJ7"
-  };
+  apiKey: 'api-key',
+  authDomain: 'project-id.firebaseapp.com',
+  databaseURL: 'https://project-id.firebaseio.com',
+  projectId: 'project-id',
+  storageBucket: 'project-id.appspot.com',
+  messagingSenderId: 'sender-id',
+  appId: 'app-id',
+  measurementId: 'G-measurement-id',
+};
 
   const firebase = initializeApp(firebaseConfig);
 
