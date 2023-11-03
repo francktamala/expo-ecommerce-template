1cf305b65dc245c2562217a76714b1bd9f229c97
francktamala <75437467+francktamala@users.noreply.github.com>
2023-11-04 01:14:35 +0200

    Update App.js

--- App.js
+++ App.js
@@ -27,16 +27,6 @@ MaterialCommunityIcons.loadFont()
 function App() {
   const { persistor, store } = storePre;
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyBW84ML6OU4_9wOR34lNK1SYftxXsrEepk",
-    authDomain: "shop-8ced3.firebaseapp.com",
-    projectId: "shop-8ced3",
-    storageBucket: "shop-8ced3.appspot.com",
-    messagingSenderId: "1090323738531",
-    appId: "1:1090323738531:web:e15112619d9266013cf416",
-    measurementId: "G-XKKRFRTZJ7"
-  };
-
   const app = firebase;
 
   return (
